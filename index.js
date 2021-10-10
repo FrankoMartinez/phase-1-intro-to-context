5adc0b7519c385866597db27da8e3946eb64b9c6
Francisco <frankmartinez4050@gmail.com>
2021-10-10 16:45:55 -0500

    partially done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let employee = {
+        'firstName': array[0],
+        'familyName': array[1],
+        'title': array[2],
+        'payPerHour': array[3],
+        'timeInEvents': [],
+        'timeOutEvents': []
+    }
+    return employee
+}
+
+function createEmployeeRecords(array) {
+    let map = array.map(x => createEmployeeRecord(x))
+    map.length
+    return map
+}
+
+function createTimeInEvent(obj, date) {
+    let timeIn = date
+    obj.timeInEvents = {
+        'type': timeIn,
+        'hour': timeIn.getHour(),
+        'date': timeIn.getDate()
+    }
+}
