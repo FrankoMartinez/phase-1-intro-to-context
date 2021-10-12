633f07b62cd2deee536dce53f29686ee1e33634d
Francisco <frankmartinez4050@gmail.com>
2021-10-12 18:08:03 -0500

    Partially done

--- index.js
+++ index.js
@@ -16,12 +16,50 @@ function createEmployeeRecords(array) {
     map.length
     return map
 }
-
+// First split the date string into 2 parts
+// "2014-02-28 1400" 
+// Once splitted take time(change time into integer from a string) and date
 function createTimeInEvent(obj, date) {
     let timeIn = date
-    obj.timeInEvents = {
-        'type': timeIn,
-        'hour': timeIn.getHour(),
-        'date': timeIn.getDate()
-    }
+    let splitDate = timeIn.indexOf(' ')
+    let time = parseInt(timeIn.substr(splitDate + 1))
+    let day = timeIn.substr(0, splitDate)
+    parseInt(day, 10)
+    obj.timeInEvents.push({
+        'type': "TimeIn",
+        'hour': time,
+        'date': day
+    })
+    return obj
+}
+
+function createTimeOutEvent(obj, date) {
+    let timeOut = date
+    let splitDate = timeOut.indexOf(' ')
+    let time = parseInt(timeOut.substr(splitDate + 1))
+    let day = timeOut.substr(0, splitDate)
+    parseInt(day, 10)
+    obj.timeOutEvents.push({
+        'type': "TimeOut",
+        'hour': time,
+        'date': day
+    })
+    return obj
+}
+
+function hoursWorkedOnDate(obj, date) {
+    let timeIn = obj.timeInEvents
+    let inDate = timeIn.find(element => element.date === date)
+    let timeOut = obj.timeOutEvents
+    let outDate = timeOut.find(element => element.date === date)
+    return (outDate.hour - inDate.hour)/ 100
+}
+
+function wagesEarnedOnDate(obj, date) {
+    return (hoursWorkedOnDate(obj, date) * obj.payPerHour)
+}
+
+function allWagesFor(obj) {
+    console.log(Object.values(obj.timeInEvents))
+    return wagesEarnedOnDate(obj) * obj.timeInEvents[0].date
 }
